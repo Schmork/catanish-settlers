68dbefd0aa5e5dfb8da332e73b4eed771e29cf54
JonnyTieM <JonnyTieM@users.noreply.github.com>
2015-03-31 16:31:43 +0200

    Interface for Map, so we know which functions we are going to need.

---
+++ src/de/htwg/se/catanishsettlers/modules/map/IMap.java
@@ -0,0 +1,40 @@
+package de.htwg.se.catanishsettlers.modules.map;
+
+/**
+ * Interface for class Map.
+ * Created by JonnyTieM on 31.03.2015.
+ */
+public interface IMap {
+    /**
+     * This returns all edges of a Field in following order: top, top right, bottom right, bottom, bottom left, top left
+     *
+     * @param field
+     * @return all edges of the "field"
+     */
+    public Edge[] getEdges(Field field);
+
+    /**
+     * This returns all vertices of a Field in following order: top left, top right, middle right, bottom right, bottom left, middle left.
+     *
+     * @param field
+     * @return all vertices of the "field"
+     */
+    public Vertex[] getVertices(Field field);
+
+    /**
+     * This returns all adjacent fields to a vertex. No specific order. This is for example useful for finding out the resources a settlement gets.
+     *
+     * @param vertex
+     * @return adjacent fields to the vertex
+     */
+    public Field[] getAdjacentFields(Vertex vertex);
+
+    /**
+     * This returns all neighbouring vertices of the given vertex. No specific order.
+     * This might be useful for example for checking whether you are allowed to build a settlement on this vertex.
+     *
+     * @param vertex
+     * @return neighbouring vertices to given vertex
+     */
+    public Vertex[] getNeighbouringVertices(Vertex vertex);
+}
