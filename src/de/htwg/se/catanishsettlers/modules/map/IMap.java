a00372e4dae11deb77c988ecb21bf3881110d777
JonnyTieM <JonnyTieM@users.noreply.github.com>
2015-03-31 16:33:54 +0200

    added description to @param

--- src/de/htwg/se/catanishsettlers/modules/map/IMap.java
+++ src/de/htwg/se/catanishsettlers/modules/map/IMap.java
@@ -8,7 +8,7 @@ public interface IMap {
     /**
      * This returns all edges of a Field in following order: top, top right, bottom right, bottom, bottom left, top left
      *
-     * @param field
+     * @param field Field you want to know the edges of
      * @return all edges of the "field"
      */
     public Edge[] getEdges(Field field);
@@ -16,7 +16,7 @@ public interface IMap {
     /**
      * This returns all vertices of a Field in following order: top left, top right, middle right, bottom right, bottom left, middle left.
      *
-     * @param field
+     * @param field Field you want to know the vertices of
      * @return all vertices of the "field"
      */
     public Vertex[] getVertices(Field field);
@@ -24,7 +24,7 @@ public interface IMap {
     /**
      * This returns all adjacent fields to a vertex. No specific order. This is for example useful for finding out the resources a settlement gets.
      *
-     * @param vertex
+     * @param vertex Vertex you want to know the adjacent fields of
      * @return adjacent fields to the vertex
      */
     public Field[] getAdjacentFields(Vertex vertex);
@@ -33,7 +33,7 @@ public interface IMap {
      * This returns all neighbouring vertices of the given vertex. No specific order.
      * This might be useful for example for checking whether you are allowed to build a settlement on this vertex.
      *
-     * @param vertex
+     * @param vertex Vertex you want to know the neighbouring vertices of
      * @return neighbouring vertices to given vertex
      */
     public Vertex[] getNeighbouringVertices(Vertex vertex);
