af1fb734e0a5a5ed6ec1c5e7c7b8280aee456faa
JonnyTieM <JonnyTieM@users.noreply.github.com>
2015-03-31 16:33:33 +0200

    added links we got inpiration from

--- src/de/htwg/se/catanishsettlers/modules/map/Map.java
+++ src/de/htwg/se/catanishsettlers/modules/map/Map.java
@@ -2,6 +2,10 @@ package de.htwg.se.catanishsettlers.modules.map;
 
 /**
  * Map manages the fields, edges and vertices of the board. It knows to which field each vetix or edge belongs.
+ *
+ * Some inspiration for this class is taken from following links:
+ * http://www.redblobgames.com/grids/hexagons/
+ * http://stackoverflow.com/questions/5040295/data-structure-for-settlers-of-catan-map
  * Created by JonnyTieM on 29.03.2015.
  */
 public class Map {
