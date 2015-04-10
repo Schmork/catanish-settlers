fe5b51b45b4ef953e79f00082c434709ce2cd135
JonnyTieM <JonnyTieM@users.noreply.github.com>
2015-04-10 11:38:50 +0200

    implemented getEdges(Field field)

--- src/de/htwg/se/catanishsettlers/modules/map/Map.java
+++ src/de/htwg/se/catanishsettlers/modules/map/Map.java
@@ -4,7 +4,7 @@ package de.htwg.se.catanishsettlers.modules.map;
  * Map manages the fields, edges and vertices of the board. It knows to which field each vetix or edge belongs.
  * x coordinates start on the left with zero and go up towards the right.
  * y coordinates start on the top with zero and to up towards the bottom.
- * <p>
+ * <p/>
  * Some inspiration for this class is taken from following links:
  * http://www.redblobgames.com/grids/hexagons/
  * http://stackoverflow.com/questions/5040295/data-structure-for-settlers-of-catan-map
@@ -50,9 +50,19 @@ public final class Map implements IMap {
         Edge[] edges = new Edge[6];
 
         if (x % 2 == 0) {
-            edges[1] = this.edges[x][3 + y]; //top
+            edges[1] = this.edges[x][3 * y]; // top
+            edges[2] = this.edges[x + 1][3 * y + 1]; // top right
+            edges[3] = this.edges[x + 1][3 * y + 2]; // bottom right
+            edges[4] = this.edges[x][3 * y + 3]; // bottom
+            edges[5] = this.edges[x][3 * y + 2]; // bottom left
+            edges[6] = this.edges[x][3 * y + 1]; // top left
         } else {
-
+            edges[1] = this.edges[x][3 * y]; // top
+            edges[2] = this.edges[x + 1][3 * y + 2]; // top right
+            edges[3] = this.edges[x + 1][3 * y + 4]; // bottom right
+            edges[4] = this.edges[x][3 * y + 3]; // bottom
+            edges[5] = this.edges[x][3 * y + 4]; // bottom left
+            edges[6] = this.edges[x][3 * y + 2]; // top left
         }
 
         return edges;
@@ -71,14 +81,14 @@ public final class Map implements IMap {
         Vertex[] vertices = new Vertex[6];
 
         if (x % 2 == 0) {
-            vertices[0] = this.vertices[x][2 * y]; //top left
+            vertices[0] = this.vertices[x][2 * y]; // top left
             vertices[1] = this.vertices[x + 1][2 * y]; // top right
             vertices[2] = this.vertices[x + 1][2 * y + 1]; // middle right
             vertices[3] = this.vertices[x + 1][2 * y + 2]; // bottom right
             vertices[4] = this.vertices[x][2 * y + 2]; // bottom left
             vertices[5] = this.vertices[x][2 * y + 1]; // middle left
         } else {
-            vertices[0] = this.vertices[x][2 * y + 1]; //top left
+            vertices[0] = this.vertices[x][2 * y + 1]; // top left
             vertices[1] = this.vertices[x + 1][2 * y + 1]; // top right
             vertices[2] = this.vertices[x + 1][2 * y + 2]; // middle right
             vertices[3] = this.vertices[x + 1][2 * y + 3]; // bottom right
