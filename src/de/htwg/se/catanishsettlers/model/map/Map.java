66ecfc0d75ea643fc136bf783f9def320e7fb815
JonnyTieM <JonnyTieM@users.noreply.github.com>
2015-04-24 10:11:30 +0200

    removed redundancee

--- src/de/htwg/se/catanishsettlers/model/map/Map.java
+++ src/de/htwg/se/catanishsettlers/model/map/Map.java
@@ -4,7 +4,7 @@ package de.htwg.se.catanishsettlers.model.map;
  * Map manages the fields, edges and vertices of the board. It knows to which field each vetix or edge belongs.
  * x coordinates start on the left with zero and go up towards the right.
  * y coordinates start on the top with zero and to up towards the bottom.
- * <p/>
+ * <p>
  * Some inspiration for this class is taken from following links:
  * http://www.redblobgames.com/grids/hexagons/
  * http://stackoverflow.com/questions/5040295/data-structure-for-settlers-of-catan-map
@@ -68,29 +68,35 @@ public final class Map implements IMap {
         }
     }
 
+    /**
+     * Returns Field of given position.
+     *
+     * @param x x-Coordinate
+     * @param y y-Coordinate
+     * @return Field at given position
+     */
+    public Field getField(int x, int y) {
+        return fields[x][y];
+    }
+
     @Override
     public Edge[] getEdges(Field field) {
-        int x = field.getX();
-        int y = field.getY();
+        if (field == null) {
+            return null;
+        }
 
         // Create edges array, where we put in all the edges of the field and then return it.
         Edge[] edges = new Edge[6];
-        //TODO: remove redundancy
-        if (x % 2 == 0) {
-            edges[1] = this.edges[x][3 * y]; // top
-            edges[2] = this.edges[x + 1][3 * y + 1]; // top right
-            edges[3] = this.edges[x + 1][3 * y + 2]; // bottom right
-            edges[4] = this.edges[x][3 * y + 3]; // bottom
-            edges[5] = this.edges[x][3 * y + 2]; // bottom left
-            edges[6] = this.edges[x][3 * y + 1]; // top left
-        } else {
-            edges[1] = this.edges[x][3 * y]; // top
-            edges[2] = this.edges[x + 1][3 * y + 2]; // top right
-            edges[3] = this.edges[x + 1][3 * y + 4]; // bottom right
-            edges[4] = this.edges[x][3 * y + 3]; // bottom
-            edges[5] = this.edges[x][3 * y + 4]; // bottom left
-            edges[6] = this.edges[x][3 * y + 2]; // top left
-        }
+
+        int[] x = getEdgesCoordinateX(field);
+        int[] y = getEdgesCoordinateY(field);
+
+        edges[1] = this.edges[x[0]][y[0]]; // top
+        edges[2] = this.edges[x[1]][y[1]]; // top right
+        edges[3] = this.edges[x[2]][y[2]]; // bottom right
+        edges[4] = this.edges[x[3]][y[3]]; // bottom
+        edges[5] = this.edges[x[4]][y[4]]; // bottom left
+        edges[6] = this.edges[x[5]][y[5]]; // top left
 
         return edges;
     }
@@ -155,28 +161,18 @@ public final class Map implements IMap {
             return null;
         }
 
-        int x = field.getX();
-        int y = field.getY();
-
         // Create vertex array, where we put in all the vertices of the field and then return it.
         Vertex[] vertices = new Vertex[6];
 
-        //TODO: remove redundancy
-        if (x % 2 == 0) {
-            vertices[0] = this.vertices[x][2 * y]; // top left
-            vertices[1] = this.vertices[x + 1][2 * y]; // top right
-            vertices[2] = this.vertices[x + 1][2 * y + 1]; // middle right
-            vertices[3] = this.vertices[x + 1][2 * y + 2]; // bottom right
-            vertices[4] = this.vertices[x][2 * y + 2]; // bottom left
-            vertices[5] = this.vertices[x][2 * y + 1]; // middle left
-        } else {
-            vertices[0] = this.vertices[x][2 * y + 1]; // top left
-            vertices[1] = this.vertices[x + 1][2 * y + 1]; // top right
-            vertices[2] = this.vertices[x + 1][2 * y + 2]; // middle right
-            vertices[3] = this.vertices[x + 1][2 * y + 3]; // bottom right
-            vertices[4] = this.vertices[x][2 * y + 3]; // bottom left
-            vertices[5] = this.vertices[x][2 * y + 2]; // middle left
-        }
+        int[] x = getVerticesCoordinateX(field);
+        int[] y = getVerticesCoordinateY(field);
+
+        vertices[0] = this.vertices[x[0]][y[0]]; // top left
+        vertices[1] = this.vertices[x[1]][y[1]]; // top right
+        vertices[2] = this.vertices[x[2]][y[2]]; // middle right
+        vertices[3] = this.vertices[x[3]][y[3]]; // bottom right
+        vertices[4] = this.vertices[x[4]][y[4]]; // bottom left
+        vertices[5] = this.vertices[x[5]][y[5]]; // middle left
 
         return vertices;
     }
