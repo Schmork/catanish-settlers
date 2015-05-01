e61d934e1afa8492d3d296bf978eb2a9c41ea479
JonnyTieM <JonnyTieM@users.noreply.github.com>
2015-05-01 12:04:46 +0200

    created Test class for Map

---
+++ test/de/htwg/se/catanishsettlers/model/map/MapTest.java
@@ -0,0 +1,53 @@
+package de.htwg.se.catanishsettlers.model.map;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by JonnyTieM on 01.05.2015.
+ */
+public class MapTest {
+    Map map;
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testGetField() throws Exception {
+
+    }
+
+    @Test
+    public void testGetEdge() throws Exception {
+
+    }
+
+    @Test
+    public void testGetVertex() throws Exception {
+
+    }
+
+    @Test
+    public void testGetEdges() throws Exception {
+
+    }
+
+    @Test
+    public void testGetVertices() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAdjacentFields() throws Exception {
+
+    }
+
+    @Test
+    public void testGetNeighbouringVertices() throws Exception {
+
+    }
+}
